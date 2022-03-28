400570c81f219e8119b47af1dec5be2b96fca103
Vlad Kravchenko <vladkravchenkomail@gmail.com>
2022-03-28 13:17:52 +0100

    new content

--- escaper.py
+++ escaper.py
@@ -5,14 +5,10 @@ def escaping(string):
 
 string = """
 
-Державні ресурси
-Багато корисної інформації є на ірландських державних сайтах. Радимо їх читати, дещо, навіть, перекладено українською мовою.
-Головний державний сайт https://www.gov.ie/en/campaigns/bc537-irelands-response-to-the-situation-in-ukraine/
-Інформація для громадян, резидентів та біженців https://www.citizensinformation.ie/en/moving_country/asylum_seekers_and_refugees/the_asylum_process_in_ireland/coming_to_ireland_from_ukraine.html
-Сайт іміграційної служби https://www.irishimmigration.ie/faqs-for-ukraine-nationals-and-residents-of-ukraine/
-Сайт ради біженців https://www.irishrefugeecouncil.ie/ukraine-information-note?fbclid=IwAR1m2kXRTluWYSlCCz2fdWErzAH9wrJ6c7jhAzy7Xe7r98tOkO5giMXH-aA
-Посольство України в Ірландії  https://ireland.mfa.gov.ua/ 
-Сайт Українського Кризового Центру. На ньому вже є безліч інформації для приїжджих до Ірландії, включаючи як, що і де купити, посилання на групи в WhatsApp і канали в Telegram, де ви можете безпосередньо зв'язатися з іншими людьми, які приїхали з України – www.iamukrainian.ie 
+Паляниця - центр благодійності в Дубліні, де можна отримати одяг, іграшки та речі першої необхідності БЕЗКОШТОВНО. При вході в магазин перевіряють, коли ви прибули, тому треба мати документ, який підтверджує дату прибуття  - 44 Clarendon Street, D2, Dublin https://goo.gl/maps/kBapwRtvxhhhiu4h6 
+ЛімерикSacred Heart Catholic Church
+(061) 315 812, https://maps.app.goo.gl/bvim45rFfHSKmRLr7 
+До 17:00 в будні, на вихідним - під час служіння. 
 
 
 """
